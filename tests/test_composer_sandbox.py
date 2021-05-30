d541b56e7c1a32ada3b4b5cb30e0bd86ba6d7b5b
Soichi Sumi <soichi.sumi@gmail.com>
2021-05-31 01:01:52 +0900

    add dag loader test

--- tests/test_composer_sandbox.py
+++ tests/test_composer_sandbox.py
@@ -1,5 +1,15 @@
-from composer_sandbox import __version__
+from os import path
+from airflow import models
 
+DAG_DIR = path.join(path.dirname(__file__), "..", "composer_sandbox")
 
-def test_version():
-    assert __version__ == '0.1.0'
+# ref: https://github.com/godatadriven/airflow-testing-examples/blob/master/tests/dags/test_dag_integrity.py
+# ref2: https://airflow.apache.org/docs/apache-airflow/1.10.15/best-practices.html#testing-a-dag
+def test_dags():
+    dagbag = models.DagBag(dag_folder=DAG_DIR, include_examples=False)
+
+    assert dagbag.import_errors == {}
+    assert len(dagbag.dags) is 2
+
+    for dag in dagbag.dags:
+        assert dag is not None
---
+++ tests/test_dag-w-template.py
@@ -0,0 +1,5 @@
+from composer_sandbox import __version__
+
+
+def test_dag():
+    assert __version__ == '0.1.0'
